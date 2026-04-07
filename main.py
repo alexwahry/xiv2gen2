e68107ed887332c5d1b1d05e342dea54bb7a07e7
alexwahry <204044518+alexwahry@users.noreply.github.com>
2026-04-07 04:04:35 +0300

    fix

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from secrets_managment import *
 from network_utils import *
 from totp import *
 
-totp_secret = extract_secret()
-totp_code = totp_decode(totp_secret)
-
-print(totp_code)
-send_code_to_xivlauncher(totp_code)
+if __name__ == "__main__":
+    totp_secret = extract_secret()
+    totp_code = totp_decode(totp_secret)
+    
+    print(totp_code)
+    
+    send_code_to_xivlauncher(totp_code)
